9452d04b7981de1ad70a6c4c368d30764efca32d
Shahilgope1520 <111412027+Shahilgope1520@users.noreply.github.com>
2024-09-20 08:38:41 +0530

    ep-4 working with get post delete method and there orders

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
-app.use((req,res)=>{
-    res.send("hello bhai")
-})
+// app.use("/", (req, res,next) => { // order is important
+//   res.send("hello bhai");
+// });
+// app.use("/test/2", (req, res, next) => {
+//   res.send("testing 2");
+// });
+// app.use("/test", (req, res) => {   // whatever exact string string it first take it will follow
+//   res.send("testing");
+// });
+// app.use("/test/3", (req, res) => {
+//   res.send("testing 3");
+// });
 
-app.listen(7777,()=>{
-    console.log("Server is running on port 7777")
-})
+// app.use("/",(req,res)=>{
+//     res.send("HAHAHAHA")
+// })
+
+app.post("/user", (req, res) => {
+  res.send("Data Sent Succesfully");
+});
+app.delete("/user", (req, res) => {
+  res.send("Data Deleted Succesfully");
+});
+
+app.listen(7777, () => {
+  console.log("Server is running on port 7777");
+});
+
