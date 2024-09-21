88accef0ca294231256c6848c4b239eacde2ea60
Shahilgope1520 <111412027+Shahilgope1520@users.noreply.github.com>
2024-09-21 13:42:24 +0530

    ep-5 Route handlers

--- src/app.js
+++ src/app.js
@@ -30,7 +30,6 @@ app.listen(7777, () => {
   console.log("Server is running on port 7777");
 });
 
-
 // app.get("/a(bc)*c", (req, res) => {
 //     res.send({
 //       name: "Shahil",
@@ -50,20 +49,60 @@ app.listen(7777, () => {
 //     });
 //   });
 
+// app.get("/user/:id/:name", (req, res) => {
+//   console.log('dynamic params', req?.params)
+//   res.send({
+//     name: "Shahil",
+//     role: "F.E.",
+//   });
+// });
+// app.get("/user", (req, res) => {  //http://localhost:7777/user?id=200&name=shahil
+//   console.log('query params', req?.query)
+//   res.send({
+//     name: "Shahil",
+//     role: "F.E.",
+//   });
+// });
 
-  app.get("/user/:id/:name", (req, res) => {
-    console.log('dynamic params', req?.params)
-    res.send({
-      name: "Shahil",
-      role: "F.E.",
-    });
-  });
-  app.get("/user", (req, res) => {  //http://localhost:7777/user?id=200&name=shahil
-    console.log('query params', req?.query)
-    res.send({
-      name: "Shahil",
-      role: "F.E.",
-    });
-  });
+//ep-5 middlewear and error handlers
 
+app.use(
+  "/users",
+  (req, res, next) => {
+    res.send("1");
+    next();
+    console.log("1");
+  },
+  (req, res, next) => {
+    next();
+    console.log("2");
+  },
+  [
+    (req, res, next) => {
+      next();
+      console.log("3");
+    },
+    (req, res, next) => {
+      next();
+      console.log("4");
+    },
+  ],
+  (req, res, next) => {
+    next();
+    console.log("5");
+  }
+);
 
+app.use(
+  "/test",
+  (req, res, next) => {
+    next();
+    res.send("1");
+    console.log("1");
+  },
+  (req, res, next) => {
+    next();
+    res.send("2"); // in this case 2 one will sent and give error for sending response again of 1
+    console.log("2");
+  }
+);
