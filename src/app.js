cbb300271273b51b988751759d390a534e1b96de
Shahilgope1520 <111412027+Shahilgope1520@users.noreply.github.com>
2024-09-22 22:52:09 +0530

    ep-05 error handelling middleware

--- src/app.js
+++ src/app.js
@@ -1,21 +1,18 @@
 const express = require("express");
-const { adminAuth, authUser } = require("./Authorization/auth");
 
 const app = express();
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
 });
 
-app.use("/admin", adminAuth);
+app.get("/getUserData",(req,res)=>{
+  throw new Error("hello")
+})
+app.use("/",(err,req,res,next)=>{
+  if(err){
+    console.error('err', err.message)
+    res.status(500).send(err.message)
+  }
+})
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("sent all data of admin");
-});
 
-app.post("/admin/deleteUser", (req, res) => {
-  res.send("Deleted User Succesfully");
-});
-
-app.get("/user", authUser, (req, res) => {
-  res.send("Get User Data");
-});
