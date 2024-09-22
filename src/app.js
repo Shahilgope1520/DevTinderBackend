0dcdb210c3b54de83b26fb70c32b4364c45d5e54
Shahilgope1520 <111412027+Shahilgope1520@users.noreply.github.com>
2024-09-22 22:56:44 +0530

    ep-05 auth user except login user

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 const express = require("express");
+const {authUser} = require("./Authorization/auth")
 
 const app = express();
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
 });
 
-app.get("/getUserData",(req,res)=>{
-  throw new Error("hello")
-})
-app.use("/",(err,req,res,next)=>{
-  if(err){
-    console.error('err', err.message)
-    res.status(500).send(err.message)
-  }
+app.get("/user/login",(req,res)=>{
+  res.send("Logged in succesfully")
 })
 
+app.use("/user",authUser,(req,res)=>{
+  res.send("User data")
+})
 
